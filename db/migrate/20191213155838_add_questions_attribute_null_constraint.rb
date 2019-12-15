d6369e5e479c0ab5d8df65369615cac3cb64dec9
LDN <ledenevdn@gmail.com>
2019-12-15 21:05:06 +0700

    Update replies

--- db/migrate/20191213155838_add_questions_attribute_null_constraint.rb
+++ db/migrate/20191213155838_add_questions_attribute_null_constraint.rb
@@ -1,6 +1,5 @@
 class AddQuestionsAttributeNullConstraint < ActiveRecord::Migration[6.0]
   def change
     change_column_null(:questions, :body, false)
-    add_column(:questions, :correct, :boolean, default: false)
   end
 end
--- db/migrate/20191213160017_add_replies_attribute_null_constraint.rb
+++ db/migrate/20191213160017_add_replies_attribute_null_constraint.rb
@@ -1,5 +1,6 @@
 class AddRepliesAttributeNullConstraint < ActiveRecord::Migration[6.0]
   def change
     change_column_null(:replies, :body, false)
+    add_column(:replies, :correct, :boolean, default: false)
   end
 end
