c1ce1e3f3fb0a3e2f9a5d3d404a8582ae4b3556e
LDN <ledenevdn@gmail.com>
2019-12-16 15:55:35 +0700

    Add replies migration

--- db/migrate/20191213160017_add_replies_attribute_null_constraint.rb
+++ db/migrate/20191213160017_add_replies_attribute_null_constraint.rb
@@ -1,6 +1,5 @@
 class AddRepliesAttributeNullConstraint < ActiveRecord::Migration[6.0]
   def change
     change_column_null(:replies, :body, false)
-    add_column(:replies, :correct, :boolean, default: false)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_160321) do
+ActiveRecord::Schema.define(version: 2019_12_16_085223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_160321) do
     t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "correct", default: false
   end
 
   create_table "tests", force: :cascade do |t|
