7d7e3234fc7e11aacf6d5ab9aaefb94b863abea7
LDN <ledenevdn@gmail.com>
2019-12-24 22:40:04 +0700

    Add metods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
 
-  def self.category_tests(category)
-
+  def self.category_tests(title)
+    Category.joins('JOIN categories ON tests.category_id = categories.id')
+        .where(categories: { title: title })
+        .order(title: :desc)
+        .pluck(:title)
   end
 end
