065f1dc5546d2a13d8e405bc1c5056a8f0176e45
LDN <ledenevdn@gmail.com>
2020-01-23 13:28:08 +0700

    Update associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :test_users
   has_many :users, through: :test_users
+  belongs_to :author, class_name: 'User'
 
   def self.category_tests(title)
     joins('JOIN categories ON categories.id = tests.category_id').where('categories.title = ?', title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :test_users
   has_many :tests, through: :test_users
+  has_many :author_tests, class_name: 'Test', foreign_key: 'author_id'
 
   def passed_tests_level(level)
     tests.where('level = ?, level')
