a968ed62c9064b0fdea187579c3754b011963094
LDN <ledenevdn@gmail.com>
2020-02-12 19:15:45 +0700

    Create question helper

--- app/helpers/footer_helper.rb
+++ app/helpers/footer_helper.rb
@@ -1,8 +1,10 @@
+module FooterHelper
+  def github_url(author, repo)
+    link_to 'TestGuru', 'https://github.com/' + author.to_s + '/' + repo.to_s,
+                                                   target: '_blank', rel: 'nofollow'
+  end
 
-def github_url(author, repo)
-  link_to 'TestGuru', 'https://github.com/' + author.to_s + '/' + repo.to_s, target: '_blank', rel: 'nofollow'
-end
-
-def current_year
-  Time.now.year
+  def current_year
+    Time.now.year
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(question)
+    if question.persisted?
+      "Edit #{@question.test.title} Question"
+    else
+      "Create New #{@test.title} Question"
+    end
+  end
 end
