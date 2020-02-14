2022da79daaf2354d0e2717ebb05d221498db7da
LDN <ledenevdn@gmail.com>
2020-02-14 22:58:17 +0700

    Fixed helper

--- app/helpers/footer_helper.rb
+++ app/helpers/footer_helper.rb
@@ -1,6 +1,6 @@
 module FooterHelper
   def github_url(author, repo)
-    link_to 'TestGuru', "https://github.com/#{author+repo}", target: '_blank', rel: 'nofollow'
+    link_to 'TestGuru', "https://github.com/#{author+'/'+repo}", target: '_blank', rel: 'nofollow'
   end
 
   def current_year
