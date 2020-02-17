cf0afd177b238fec903268aed4b8d7ba6e3d2896
LDN <ledenevdn@gmail.com>
2020-02-17 16:42:06 +0700

    Add actions test controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,14 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update destroy]
 
   def index
     @tests = Test.all
   end
 
   def show
-    @test = Test.find(params[:id])
+  end
+
+  def edit
   end
 
   def new
@@ -21,10 +24,27 @@ class TestsController < ApplicationController
     end
   end
 
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
   private
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 
 end
