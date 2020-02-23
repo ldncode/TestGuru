41defbbdd440f290b017f53a8c05454e955ecf09
LDN <ledenevdn@gmail.com>
2020-02-23 17:01:42 +0700

    Add update model

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -31,7 +31,7 @@ class TestPassage < ApplicationRecord
   end
 
   #correct_answers.ids.sort == answer_ids.map(&:to_i).sort
-  
+
   def correct_answer?(answer_ids)
     (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
     correct_answers_count == answer_ids.count
