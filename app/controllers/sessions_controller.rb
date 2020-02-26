a3158fe0b2aefa8fbce05294b7359f7b2ac504f1
LDN <ledenevdn@gmail.com>
2020-02-26 15:52:13 +0700

    Add logout user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    User.find(session[:user_id]).destroy
+    User.find(session[:user_id])
     session[:user_id] = nil
     redirect_to login_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
+  get :logout, to: 'sessions#destroy'
 
 
   resources :users, only: :create
