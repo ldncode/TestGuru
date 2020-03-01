c3f0439e442960c9b000b67638528f7bd679fea4
LDN <ledenevdn@gmail.com>
2020-03-01 17:23:04 +0700

    Add mailer

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,7 @@
 class TestsController < ApplicationController
+
   before_action :authenticate_user!
   before_action :find_test, only: %i[show edit update destroy start]
-  before_action :set_user, only: :start
 
   def index
     @tests = Test.all
@@ -40,8 +40,8 @@ class TestsController < ApplicationController
   end
 
   def start
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
@@ -54,8 +54,5 @@ class TestsController < ApplicationController
     @test = Test.find(params[:id])
   end
 
-  def set_user
-    @user = User.first
-  end
 
 end
