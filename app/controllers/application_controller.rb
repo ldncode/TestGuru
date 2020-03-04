cce3cc4d512a80b426d958521a5425ff05dbef78
LDN <ledenevdn@gmail.com>
2020-03-04 14:12:32 +0700

    Add helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+
+  def after_sign_in_path_for(resource)
+    flash[:alert] = "Hello, #{current_user.last_name}!" if current_user.last_name
+    resource.is_a?(Admin) ? admin_tests_path : root_path
+  end
 
 
 end
