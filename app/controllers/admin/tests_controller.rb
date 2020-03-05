fa03aa43b468bd6126c4b2eaeba3a7ec830f6511
LDN <ledenevdn@gmail.com>
2020-03-06 00:09:13 +0700

    Localization view in screencast

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -17,7 +17,7 @@ class Admin::TestsController < Admin::BaseController
   def create
     @test = current_user.author_tests.new(test_params)
     if @test.save
-      redirect_to admin_tests_path
+      redirect_to admin_tests_path, notice: t('.success')
     else
       render :new
     end
