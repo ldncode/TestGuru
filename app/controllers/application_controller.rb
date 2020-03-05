a1044fd9a462c417345db4aec7f593c7a13a2d9f
LDN <ledenevdn@gmail.com>
2020-03-05 20:52:59 +0700

    Add localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   protected
 
@@ -11,6 +16,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     flash[:alert] = "Hello, #{current_user.first_name}!"
     resource.admin? ? admin_tests_path : root_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 
