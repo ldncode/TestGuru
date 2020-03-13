0a8592fb0586220a2834dba62841aa65d592dc5f
LDN <ledenevdn@gmail.com>
2020-03-13 14:32:35 +0700

    Add flash messages bootstrap alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
+
   def default_url_options
     I18n.locale == I18n.default_locale ? { lang: nil } : { lang: I18n.locale }
   end
