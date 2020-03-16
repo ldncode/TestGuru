811107fb5600a5c991c0e0e8d63cf902f744e0ce
LDN <ledenevdn@gmail.com>
2020-03-16 16:31:02 +0700

    Update finish

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -8,7 +8,7 @@ class Admin::BaseController < ApplicationController
   private
 
   def admin_required!
-    redirect_to root_path, alert: 'You are not authorized to view this page.' unless current_user.is_a?(Admin)
+    redirect_to root_path, alert: 'You are not authorized to view this page.' unless current_user.admin?
   end
 
 end
