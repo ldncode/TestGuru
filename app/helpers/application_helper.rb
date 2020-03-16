9d421cd273cc3ef6056d366f89b32f194996197e
LDN <ledenevdn@gmail.com>
2020-03-16 22:56:41 +0700

    Add api clients

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,21 @@
 module ApplicationHelper
 
+  # def flash_class(level)
+  #   case level
+  #   when 'notice' then "alert alert-info"
+  #   when 'success' then "alert alert-success"
+  #   when 'error' then "alert alert-danger"
+  #   when 'alert' then "alert alert-warning"
+  #   end
+  # end
+
+  MESSAGE = { notice: 'alert alert-info', success: 'alert alert-success',
+            error: 'alert alert-danger', alert: 'alert alert-warning' }
+
   def flash_class(level)
-    case level
-    when 'notice' then "alert alert-info"
-    when 'success' then "alert alert-success"
-    when 'error' then "alert alert-danger"
-    when 'alert' then "alert alert-warning"
-    end
+    MESSAGE[level]
   end
 
 end
+
+
---
+++ lib/clients/git_hub_client.rb
@@ -0,0 +1,24 @@
+class GitHubClient
+
+  ROOT_ENDPOINT = 'https://api.github.com'
+  ACCESS_TOKEN = ENV['GITHUB_TOKEN']
+
+  def initialize
+    @http_client = setup_http_client
+  end
+
+  def create_gist(params)
+    @http_client.post('gists') do |request|
+      request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
+      request.headers['Content-Type'] = 'application/json'
+      request.body = params.to_json
+    end
+  end
+
+  private
+
+  def setup_http_client
+    Faraday.new(url: ROOT_ENDPOINT)
+  end
+
+end
