6313a230233379ec6bebd7aa533feaaaf34320e0
LDN <ledenevdn@gmail.com>
2020-03-28 19:44:05 +0700

    Fix misprint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_maler.default_url_options = {}
+  config.action_mailer.default_url_options = {}
   config.action_mailer.smtp_settings = {
       address:             'smtp.gmail.com',
       port:                 587,
