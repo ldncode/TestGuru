73311c8cf58e4253a2e29019ea222cccfa19c37b
LDN <ledenevdn@gmail.com>
2020-03-28 19:24:25 +0700

    Add production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :file
   config.action_maler.default_url_options = {}
   config.action_mailer.smtp_settings = {
       address:             'smtp.gmail.com',
