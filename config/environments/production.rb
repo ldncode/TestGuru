4d43535c90e43d80493c8917ddeb29f07124a6dd
LDN <ledenevdn@gmail.com>
2020-04-01 23:49:09 +0700

    Add host production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {}
+  config.action_mailer.default_url_options = { host: 'frozen-shelf-13237.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       address:             'smtp.gmail.com',
       port:                 587,
