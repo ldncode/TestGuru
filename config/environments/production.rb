01a5bae89e6953558612b442258b8fd3f7af9064
LDN <ledenevdn@gmail.com>
2020-04-03 13:23:00 +0700

    Add domain production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:             'smtp.gmail.com',
       port:                 587,
+      domain:              'frozen-shelf-13237.herokuapp.com',
       user_name:            ENV['SMTP_USERNAME'],
       password:             ENV['SMTP_PASSWORD'],
       authentication:      'plain',
