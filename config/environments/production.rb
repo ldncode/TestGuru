566135ca6d7304617bc20cf906cf02e01223f1a3
LDN <ledenevdn@gmail.com>
2020-04-03 14:09:56 +0700

    Heroku name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'frozen-shelf-13237.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'gurutests.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       address:             'smtp.gmail.com',
       port:                 587,
-      domain:              'frozen-shelf-13237.herokuapp.com',
+      domain:              'gurutests.herokuapp.com',
       user_name:            ENV['SMTP_USERNAME'],
       password:             ENV['SMTP_PASSWORD'],
       authentication:      'plain',
