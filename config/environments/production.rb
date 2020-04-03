b1b8402c9c41c7b228e55f854888c964273700f1
LDN <ledenevdn@gmail.com>
2020-04-03 14:47:52 +0700

    Update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'gurutests.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       address:             'smtp.gmail.com',
