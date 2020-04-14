5f7f03a63b5288cddcb74f02275533f683e4544d
LDN <ledenevdn@gmail.com>
2020-04-14 15:50:53 +0700

    Add models

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,7 @@
 class Badge < ApplicationRecord
+  has_and_belongs_to_many :users
+  has_one :rule, class_name: 'BadgeRule', dependent: :destroy
+
+  validates :name, presence: true
+  validates :image, presence: true
 end
